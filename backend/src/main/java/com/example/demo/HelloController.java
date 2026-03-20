f77fce1566ac0ca1d659abfb401bf5ce013377e6
Jyothi Swaroop Marri <jyothiswaroop.marri@pal.tech>
2026-03-20 12:06:47 +0530

    backend commit

--- backend/src/main/java/com/example/demo/HelloController.java
+++ backend/src/main/java/com/example/demo/HelloController.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.HashMap;
 
 @RestController
+@CrossOrigin(origins = "d2dk0ou1ozpcg7.cloudfront.net")
 public class HelloController {
 
     @GetMapping("/api/hello")
